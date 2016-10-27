9093aab56aa416bd0611b607a8a9d072b6657c25
mkahihu <kingmiller12@gmail.com>
2016-10-27 10:43:43 -0400

    Update all functions
    Worked on selectColor, main, draw, and getMousePos.

--- Final.js
+++ Final.js
@@ -1,1 +1,34 @@
 /*Author: Miller Kahihu and Tiffany Tang*/
+var color = "#000000";
+function selectColor(hexcode)
+{
+    color = hexcode;
+}
+function selectBrush(brush)
+{
+
+}
+function draw
+{
+       
+}
+function getMousePos(canvas, e) 
+{
+    var rect = canvas.getBoundingClientRect();
+    return 
+    {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+    };
+}
+function main()
+{
+    var canvas = document.getElementById("myCanvas");
+    var pen = canvas.getContext("2d");
+    canvas.addEventListner("mousedown", function(e)
+    {
+        pen.fillStyle = color;
+        getMousePos(canvas, e);
+        draw();
+    });
+}
