9aad245545be90d45bb9d206eb64e840a88cf548
mkahihu <kingmiller12@gmail.com>
2016-10-30 17:53:13 -0400

    Fix everything
    HTML- removed brushes stuff, removed white color, updated input tag to have a change event
    CSS- Nothing
    JS- EVERYTHING, fixed color select, added changed color, made drawing code muy complicado, no comments (Tiffany will add)

--- Final.js
+++ Final.js
@@ -1,34 +1,91 @@
 /*Author: Miller Kahihu and Tiffany Tang*/
 var color = "#000000";
-function selectColor(hexcode)
+function selectColor(hexcode) 
 {
     color = hexcode;
+    document.getElementById("hexcode").value = color;
 }
-function selectBrush(brush)
-{
 
-}
-function draw
+function changeColor()
 {
-       
+    var hex = document.getElementById("hexcode").value;
+    if(hex.charAt(0) != "#")
+    {
+        color = "#" + hex;
+        document.getElementById("hexcode").value = color;
+    }
+    else
+    {
+        color = hex;
+    }
 }
 function getMousePos(canvas, e) 
 {
     var rect = canvas.getBoundingClientRect();
-    return 
-    {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
+    return {
+        x: Math.round((e.clientX - rect.left) / (rect.right - rect.left) * canvas.width),
+        y: Math.round((e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height)
     };
+}
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+var clickColor = new Array();
+
+function addClick(x, y, dragging)
+{
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+    clickColor.push(color)
+}
+
+function draw(canvas, pen)
+{ 
+    pen.clearRect(0, 0, canvas.width, canvas.height);
+    pen.lineJoin = "round";       
+    for(var i=0; i < clickX.length; i++) 
+    {        
+        pen.beginPath();
+        pen.lineWidth = 7;
+        if(clickDrag[i] && i)
+        {
+            pen.moveTo(clickX[i-1], clickY[i-1]);
+        }
+        else
+        {
+            pen.moveTo(clickX[i]-1, clickY[i]);
+        }
+        pen.lineTo(clickX[i], clickY[i]);
+        pen.closePath();
+        pen.strokeStyle = clickColor[i];
+        pen.stroke();
+    }
 }
 function main()
 {
-    var canvas = document.getElementById("myCanvas");
-    var pen = canvas.getContext("2d");
-    canvas.addEventListner("mousedown", function(e)
+    canvas = document.getElementById("myCanvas");
+    pen = canvas.getContext("2d");
+    canvas.addEventListener("mousedown", function(e)
+    {      
+        var mouse = getMousePos(canvas, e);
+        paint = true;
+        addClick(mouse.x, mouse.y);
+        draw(canvas, pen);
+    });
+    canvas.addEventListener("mousemove", function(e)
     {
-        pen.fillStyle = color;
-        getMousePos(canvas, e);
-        draw();
+        if(paint)
+        {
+            var mouse = getMousePos(canvas, e);
+            addClick(mouse.x, mouse.y, true);
+            draw(canvas, pen);
+        }
+    });
+    canvas.addEventListener("mouseup", function(e)
+    {
+        paint = false;
     });
 }
